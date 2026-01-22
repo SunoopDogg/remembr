cd732ce6bb3e156593b37e833778bdd554eacbff
SunoopDogg <aswoo55555@gmail.com>
2026-01-22 16:48:16 +0000

    fix: Update flash-attn installation to use prebuilt wheel

--- script/setup.sh
+++ script/setup.sh
@@ -2,10 +2,11 @@ git clone https://huggingface.co/Efficient-Large-Model/NVILA-Lite-8B
 
 source /opt/ros/humble/setup.bash
 
-source ./venv/bin/activate
-uv add flash-attn --no-build-isolation
-
 apt update
 apt install ros-humble-cv-bridge -y
 
 rm -rf /var/lib/apt/lists/*
+
+source ./venv/bin/activate
+
+uv pip install https://github.com/Dao-AILab/flash-attention/releases/download/v2.5.8/flash_attn-2.5.8+cu122torch2.3cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
